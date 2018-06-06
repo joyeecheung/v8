85a4653c8dbb526e847a70d03448fb414106878c
Joyee Cheung <joyee@igalia.com>
2018-06-07 01:05:42 +0800

    added more tests

--- test/mjsunit/harmony/symbol-description.js
+++ test/mjsunit/harmony/symbol-description.js
@@ -4,13 +4,50 @@
 
 // Flags: --harmony-symbol-description
 
-const symbol = Symbol('test');
-assertEquals(symbol.description, 'test');
+{
+  let desc = Object.getOwnPropertyDescriptor(Symbol.prototype, 'description');
+  assertEquals(desc.set, undefined);
+  assertFalse(!!desc.writable);
+  assertFalse(desc.enumerable);
+  assertTrue(desc.configurable);
+}
 
-assertFalse(symbol.hasOwnProperty('description'));
+{
+  const empty = Symbol();
+  assertEquals(empty.description, undefined);
 
-let desc = Object.getOwnPropertyDescriptor(Symbol.prototype, 'description');
-assertEquals(desc.set, undefined);
-assertFalse(!!desc.writable);
-assertFalse(desc.enumerable);
-assertTrue(desc.configurable);
+  const symbol = Symbol('test');
+  assertEquals(symbol.description, 'test');
+  assertFalse(symbol.hasOwnProperty('description'));
+}
+
+{
+  const empty = Object(Symbol());
+  assertEquals(empty.description, undefined);
+
+  const symbol = Object(Symbol('test'));
+  assertEquals(symbol.description, 'test');
+  assertFalse(symbol.hasOwnProperty('description'));
+}
+
+{
+  assertThrows(function() {
+    const proxy = new Proxy({}, {});
+    Symbol.prototype.description.call(proxy);
+  }, TypeError);
+
+  assertThrows(function() {
+    const smi = 123;
+    Symbol.prototype.description.call(smi);
+  }, TypeError);
+
+  assertThrows(function() {
+    const str = 'string';
+    Symbol.prototype.description.call(string);
+  }, TypeError);
+
+  assertThrows(function() {
+    const obj = { prop: 'test' };
+    Symbol.prototype.description.call(obj);
+  }, TypeError);
+}
