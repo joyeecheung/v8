784b169448643b41463995471c6532af33e911ad
Joyee Cheung <joyeec9h3@gmail.com>
2021-07-26 22:05:17 +0800

    add noopt and opt version for public field

--- test/js-perf-test/ClassFields/define-public-field.js
+++ test/js-perf-test/ClassFields/define-public-field.js
@@ -3,38 +3,28 @@
 // found in the LICENSE file.
 'use strict';
 
-function addBenchmark(name, test, { setup, tearDown } = {}) {
-  new BenchmarkSuite(`DefinePublic-${name}`, [1000],
-      [
-        new Benchmark(name, false, false, 0, test, setup, tearDown)
-      ]);
+const BENCHMARK_NAME = arguments[0];
+const TEST_TYPE = arguments[1];
+const optimize_param = arguments[2];
+let optimize;
+if (optimize_param == "opt") {
+  optimize = true;
+} else if (optimize_param == "noopt"){
+  optimize = false;
+} else {
+  throw new Error("Unknown optimization configuration " + arguments.join(' '));
 }
 
-addBenchmark('SingleField', SingleField, { setUp, tearDown });
-addBenchmark('MultipleFields', MultipleFields, { setUp, tearDown });
-
+let func;
 let i = 0;
-let instance;
-
-function setUp() {
-  instance = void 0;
-}
-
-function tearDown() {
-  if (!instance.check())
-    throw new Error(`Check failed`);
-}
+let array;
 
 class SingleFieldClass {
   x = i;
 
   check() {
-    return this.x === i++;
+    return this.x >= 0;
   }
-}
-
-function SingleField() {
-  return (instance = new SingleFieldClass);
 }
 
 class MultiFieldClass {
@@ -46,11 +36,56 @@ class MultiFieldClass {
   a = i+5;
 
   check() {
-    return this.x === i++ && this.y === i++ && this.z === i++ &&
-           this.q === i++ && this.r === i++ && this.a === i++;
+    return this.x + 1 === this.y && this.y + 1 === this.z &&
+           this.z + 1 === this.q && this.q + 1 === this.r &&
+           this.r + 1 === this.a;
   }
 };
 
-function MultipleFields() {
-  return (instance = new MultiFieldClass);
+switch (TEST_TYPE) {
+  case "single":
+    func = SingleFieldClass;
+    break;
+  case "multiple":
+    func = MultiFieldClass;
+    break;
+  default:
+    throw new Error("Unknown optimization configuration " + arguments.join(' '));
 }
+
+if (optimize) {
+  %PrepareFunctionForOptimization(func);
+} else {
+  %NeverOptimizeFunction(func);
+}
+
+function setUp() {
+  array = [new func(), new func()];
+  if (optimize) {
+    %OptimizeFunctionOnNextCall(func);
+  }
+}
+
+function runBenchmark() {
+  for (let i = 0; i < LOCAL_ITERATIONS; ++i) {
+    array.push(new func);
+  }
+}
+
+function tearDown() {
+  if (array.length < 3) {
+    throw new Error(`Check failed, array length ${array.length}`);
+  }
+  for (const instance of array) {
+    if (!instance.check())
+      throw new Error(`instance.check() failed`);
+  }
+}
+
+const DETERMINISTIC_RUNS = 1;
+const LOCAL_ITERATIONS = 10000;
+new BenchmarkSuite(`${BENCHMARK_NAME}`, [1000], [
+  new Benchmark(
+    `${BENCHMARK_NAME}-${TEST_TYPE}-${optimize_param}`,
+    false, false, DETERMINISTIC_RUNS, runBenchmark, setUp, tearDown)
+]);
