043de14f12d51bfcc3c9101b775ebe2814001859
Joyee Cheung <joyee@igalia.com>
2021-07-29 02:54:12 +0800

    add noopt and opt version and push the instances into array

--- test/js-perf-test/ClassFields/define-private-field.js
+++ test/js-perf-test/ClassFields/define-private-field.js
@@ -3,41 +3,31 @@
 // found in the LICENSE file.
 'use strict';
 
-function addBenchmark(name, test, { setup, tearDown } = {}) {
-  new BenchmarkSuite(`DefinePrivate-${name}`, [1000],
-      [
-        new Benchmark(name, false, false, 0, test, setup, tearDown)
-      ]);
+const BENCHMARK_NAME = arguments[0];
+const TEST_TYPE = arguments[1];
+const optimize_param = arguments[2];
+let optimize;
+if (optimize_param == "opt") {
+  optimize = true;
+} else if (optimize_param == "noopt"){
+  optimize = false;
+} else {
+  throw new Error("Unknown optimization configuration " + arguments.join(' '));
 }
 
-addBenchmark('SingleField', SingleField, { setUp, tearDown });
-addBenchmark('MultipleFields', MultipleFields, { setUp, tearDown });
-
+let func;
 let i = 0;
-let instance;
+let array;
 
-function setUp() {
-  instance = void 0;
-}
-
-function tearDown() {
-  if (!instance.check())
-    throw new Error(`Check failed`);
-}
-
-class SingleFieldClass {
+class SinglePrivateFieldClass {
   #x = i;
 
   check() {
-    return this.#x === i++;
+    return this.#x === i;
   }
 }
 
-function SingleField() {
-  return (instance = new SingleFieldClass);
-}
-
-class MultiFieldClass {
+class MultiPrivateFieldClass {
   #x = i;
   #y = i+1;
   #z = i+2;
@@ -46,10 +36,56 @@ class MultiFieldClass {
   #a = i+5;
 
   check() {
-    return this.#x === i++ && this.#y === i++ && this.#z === i++ && this.#q === i++ && this.#r === i++ && this.#a === i++;
+    return this.#x + 1 === this.#y && this.#y + 1 === this.#z &&
+           this.#z + 1 === this.#q && this.#q + 1 === this.#r &&
+           this.#r + 1 === this.#a;
   }
-};
-
-function MultipleFields() {
-  return (instance = new MultiFieldClass);
 }
+
+switch (TEST_TYPE) {
+  case "single":
+    func = SinglePrivateFieldClass;
+    break;
+  case "multiple":
+    func = MultiPrivateFieldClass;
+    break;
+  default:
+    throw new Error("Unknown optimization configuration " + arguments.join(' '));
+}
+
+if (optimize) {
+  %PrepareFunctionForOptimization(func);
+} else {
+  %NeverOptimizeFunction(func);
+}
+
+function setUp() {
+  array = [new func(), new func()];
+  if (optimize) {
+    %OptimizeFunctionOnNextCall(func);
+  }
+}
+
+function runBenchmark() {
+  for (let i = 0; i < LOCAL_ITERATIONS; ++i) {
+    array.push(new func);
+  }
+}
+
+function tearDown() {
+  if (array.length < 3) {
+    throw new Error(`Check failed, array length ${array.length}`);
+  }
+  for (const instance of array) {
+    if (!instance.check())
+      throw new Error(`instance.check() failed`);
+  }
+}
+
+const DETERMINISTIC_RUNS = 1;
+const LOCAL_ITERATIONS = 10000;
+new BenchmarkSuite(`${BENCHMARK_NAME}`, [1000], [
+  new Benchmark(
+    `${BENCHMARK_NAME}-${TEST_TYPE}-${optimize_param}`,
+    false, false, DETERMINISTIC_RUNS, runBenchmark, setUp, tearDown)
+]);
